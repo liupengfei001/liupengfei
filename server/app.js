c5505fd43410ebb4481ab5567bb946c218a315d3
刘鹏飞 <786620470@qq.com>
2016-11-01 20:29:20 +0800

    刘鹏飞上传

---
+++ server/app.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+var saticpath = path.join(__dirname,"../static");
+app.use(express.static(saticpath));
+app.listen(80,function(){
+     console.log("open http://127.0.0.1:80");
+})
